39c55f89d52b26a1b996927bf2ad1163e9b1cd94
DharaVirani <dhara28virani@gmail.com>
2023-03-16 11:42:07 +0000

    My writing homework week 1

---
+++ src/writinghw/Program2.java
@@ -0,0 +1,18 @@
+package writinghw;
+
+/**
+ * 2. Write a Java program to print a face.
+ */
+
+public class Program2 {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o  o |]");
+        System.out.println(" |  ^   | ");
+        System.out.println(" | ' '  | ");
+        System.out.println(" |  -   | ");
+        System.out.println(" +______+");
+
+
+    }
+}
